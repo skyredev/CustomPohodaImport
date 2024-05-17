33abe92dc48b6680bdeda007db7f92f8e3723e75
Skyre <skyredev@gmail.com>
2024-05-17 16:07:58 +0200

    jobs, tool

---
+++ src/backend/Tools/Pohoda/Pohoda.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Espo\Modules\PohodaImport\Tools\Pohoda;
+
+use Espo\Core\Utils\Log;
+use Espo\ORM\EntityManager;
+use Espo\Core\ORM\Entity;
+use Espo\ORM\Query\Part\Condition as Cond;
+
+class Pohoda {
+	private string $username = 'Admin';
+	private string $password = 'apertia';
+	private string $url = 'http://666.davidstrejc.cz:666/xml';
+	const DEBUG_PREFIX = '[Espo\Modules\Autocrm\Tools\Pohoda]';
+
+	public function __construct(
+		private EntityManager $entityManager,
+		private Log $log,
+	) {}
+	private function debug($message, array $context = []): void
+	{
+		$this->log->debug(self::DEBUG_PREFIX . $message, $context);
+	}
+
+	public function getIdsToSync(string $entityType): array
+	{
+		return $this->entityManager
+			->getRDBRepository($entityType)
+			->select(['id'])
+			->where(
+				Cond::Equal(Cond::column('processed'), false))
+			->find()->getValueMapList();
+	}
+	public function getEntityToSync(string $id, string $entityType): ?Entity
+	{
+		$entity = $this->entityManager
+			->getRDBRepository($entityType)
+			->where(Cond::equal(Cond::column('id'), $id))
+			->findOne();
+
+		if (!$entity) {
+			$this->debug("Entity type: {$entityType} with ID: {$id} not found");
+		}
+
+		return $entity;
+	}
+	public function sendXmlToPohoda(string $xmlData): void
+	{
+		$encodedCredentials = base64_encode("{$this->username}:{$this->password}");
+
+		$headers = [
+			'Content-Type: text/xml; charset=windows-1250',
+			'STW-Authorization: Basic ' . $encodedCredentials,
+			'Accept-Encoding: gzip, deflate',
+		];
+
+		$options = [
+			'http' => [
+				'method' => 'POST',
+				'header' => implode("\r\n", $headers),
+				'content' => iconv('UTF-8', 'Windows-1250', $xmlData),
+			],
+		];
+
+		$context = stream_context_create($options);
+		$response = file_get_contents($this->url, false, $context);
+
+		if ($response === false) {
+			throw new \Exception("Failed to send XML to Pohoda. Error: " . error_get_last()['message'] );
+		}
+
+		$statusCode = $this->getStatusCodeFromResponse($http_response_header);
+
+		if ($statusCode !== 200) {
+			throw new \Exception("Failed to send XML to Pohoda. Status code: {$statusCode}, Response: {$response}");
+		}
+	}
+
+	private function getStatusCodeFromResponse(array $responseHeaders): int
+	{
+		$statusLine = $responseHeaders[0] ?? '';
+		preg_match('/HTTP\/\d\.\d\s+(\d+)/', $statusLine, $matches);
+
+		return (int) ($matches[1] ?? 0);
+	}
+
+
+
+}
