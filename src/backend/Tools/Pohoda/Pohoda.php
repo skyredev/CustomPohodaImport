f0babbc5e7a4a0cc365cf69f90c999a0ce831945
Skyre <skyredev@gmail.com>
2024-06-09 18:12:19 +0200

    Refactors PohodaSyncSupplierInvoices.php (New ReceivedInvoices) PohodaSyncReceivedInvoices.php (Old ReceivedInvoices)
    Universal now, Integrations with Pohoda credentials TODO

--- src/backend/Tools/Pohoda/Pohoda.php
+++ src/backend/Tools/Pohoda/Pohoda.php
@@ -11,10 +11,18 @@ use Espo\ORM\Query\Part\Condition as Cond;
 use Espo\Core\ORM\Entity;
 
 class Pohoda {
-	private string $username = 'APERTIA';
-	private string $password = '12345';
-	private string $url = 'http://95.168.223.178:4444/xml';
+	private string $username = 'Admin';  //ALIS //APERTIA
+	private string $password = 'apertia'; // ALIS // 12345
+	private string $url = 'http://666.davidstrejc.cz:666/xml'; // ALIS // http://95.168.223.178:4444/xml
+	private string $headerString = '<dat:dataPack version="2.0" id="Usr01" ico="27117758" key="033efc8c-513a-4639-92fc-be4e75668d07" programVersion="13607.12 (14.3.2024)" application="Transformace" note="CRM Import" xmlns:dat="http://www.stormware.cz/schema/version_2/data.xsd">';
+
+	//AUTOCRM <dat:dataPack version="2.0" id="Usr01" ico="27117758" key="033efc8c-513a-4639-92fc-be4e75668d07" programVersion="13607.12 (14.3.2024)" application="Transformace" note="Uživatelský export" xmlns:dat="http://www.stormware.cz/schema/version_2/data.xsd">
+
+	//ALIS <dat:dataPack version="2.0" id="Usr01" ico="11223344" key="521d4e05-f032-465e-8150-f423d1b98197" programVersion="13700.208 (30.5.2024)" application="Transformace" note="Uživatelský export" xmlns:dat="http://www.stormware.cz/schema/version_2/data.xsd">
+
 	const DEBUG_PREFIX = '[Espo\Modules\PohodaImport\Tools\Pohoda]';
+
+	private array $processedEntities = [];
 
 	public function __construct(
 		private EntityManager $entityManager,
@@ -25,7 +33,7 @@ class Pohoda {
 		$this->log->debug(self::DEBUG_PREFIX . $message, $context);
 	}
 
-	public function processEntity(string $entityType, $generateXmlForEntity): void
+	public function processEntity(string $entityType, $generateXmlForEntity, $duplicityCheckFieldType = null): void
 	{
 		$entityIds = $this->getIdsToSync($entityType);
 
@@ -43,20 +51,33 @@ class Pohoda {
 					continue;
 				}
 
-				$this->debug('Trying to sync received invoice "' . $entity->get('name') . '"');
+				if ($duplicityCheckFieldType !== null) {
+					$duplicityCheckField = htmlspecialchars($entity->get($duplicityCheckFieldType));
+					if (in_array($duplicityCheckField, $this->processedEntities)) {
+						$this->debug("Entity with number: {$duplicityCheckField} already processed");
+						continue;
+					}
+				}
+
+				$this->debug('Trying to sync '.$entityType.' with name: ' . $entity->get('name'));
 
 				$xmlData = $generateXmlForEntity($entity);
 
 				$this->sendXmlToPohoda($xmlData);
 
+				if($duplicityCheckFieldType !== null) {
+					$this->processedEntities[] = $duplicityCheckField;
+				}
+
 				$entity->set('processed', true);
 				$this->entityManager->saveEntity($entity);
 
 			} catch (\Exception $exception) {
-				$this->debug('Failed to sync received invoice error msg: ' . $exception->getMessage());
+				$this->debug('Failed to sync '.$entityType.' with ID: ' . $entityId->id . '. Error: ' . $exception->getMessage());
 			}
 		}
 	}
+
 
 	public function getIdsToSync(string $entityType): array
 	{
@@ -141,11 +162,19 @@ class Pohoda {
 			'Accept-Encoding: gzip, deflate',
 		];
 
+		$xmlTemplate = trim('
+    <?xml version="1.0" encoding="Windows-1250"?>
+    ' . $this->headerString . '
+        <dat:dataPackItem version="2.0" id="Usr01 (001)">
+            ' . trim($xmlData) . '
+        </dat:dataPackItem>
+    </dat:dataPack>');
+
 		$options = [
 			'http' => [
 				'method' => 'POST',
 				'header' => implode("\r\n", $headers),
-				'content' => iconv('UTF-8', 'Windows-1250', $xmlData),
+				'content' => iconv('UTF-8', 'Windows-1250', $xmlTemplate),
 			],
 		];
 
@@ -153,15 +182,31 @@ class Pohoda {
 		$response = file_get_contents($this->url, false, $context);
 
 		if ($response === false) {
-			throw new \Exception("Failed to send XML to Pohoda. Error: " . error_get_last()['message'] );
+			throw new \Exception("Failed to send XML to Pohoda. Error: " . error_get_last()['message']);
 		}
 
-		$statusCode = $this->getStatusCodeFromResponse($http_response_header);
+		$responseHeaders = $http_response_header;
+		foreach ($responseHeaders as $header) {
+			if (stripos($header, 'Content-Encoding: gzip') !== false) {
+				$response = gzdecode($response);
+			} elseif (stripos($header, 'Content-Encoding: deflate') !== false) {
+				$response = gzinflate($response);
+			}
+		}
+
+		$statusCode = $this->getStatusCodeFromResponse($responseHeaders);
 
 		if ($statusCode !== 200) {
 			throw new \Exception("Failed to send XML to Pohoda. Status code: {$statusCode}, Response: {$response}");
 		}
+
+		$responseState = $this->getResponseState($response);
+
+		if ($responseState !== 'ok') {
+			throw new \Exception("Failed to send XML to Pohoda. Response state: {$responseState}, Response: {$response}");
+		}
 	}
+
 
 	private function getStatusCodeFromResponse(array $responseHeaders): int
 	{
@@ -171,6 +216,28 @@ class Pohoda {
 		return (int) ($matches[1] ?? 0);
 	}
 
+	private function getResponseState(string $response): string
+	{
+		try {
+			$doc = new \DOMDocument();
+			$doc->loadXML($response);
+			$xpath = new \DOMXPath($doc);
+			$xpath->registerNamespace('rsp', 'http://www.stormware.cz/schema/version_2/response.xsd');
+			$responsePack = $xpath->query('/rsp:responsePack')->item(0);
+
+			if ($responsePack) {
+				$state = $responsePack->getAttribute('state');
+				$this->debug('Parsed response state: ' . $state);
+				return $state;
+			} else {
+				$this->debug('Failed to find responsePack element in XML.');
+				return '';
+			}
+		} catch (\Exception $e) {
+			$this->debug('Failed to parse response XML: ' . $e->getMessage());
+			return '';
+		}
+	}
 
 
 }
